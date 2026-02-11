fa78ecbda56938204242d0fca5a04f3f029bb8d7
Patrick Welsh <pjwelsh1302@outlook.com>
2026-02-11 22:12:46 +0000

    Added checks for nested positioning data in JSON telemetry

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import json
 with open("telemetry/telemetry.json", "r") as file:
     telemetry_data = json.load(file)
 
-satellite_ids = ["HZN-001","HZN-002","HZN-003","HZN-004"]
-required_field = ["temperature","timestamp","voltage","age"]
+satellite_ids = ["HZN-001", "HZN-002", "HZN-003", "HZN-004"]
+required_fields = ["temperature", "timestamp", "voltage", "position"]
 
 # Check that the satellite is present in the telemetry data
 # If it is, print that it is present, if not, print absent
@@ -14,9 +14,17 @@ required_field = ["temperature","timestamp","voltage","age"]
 for satellite_id in satellite_ids:
     if satellite_id in telemetry_data:
         print("\n", satellite_id, "present")
-        for field in required_field:
+        for field in required_fields:
             if field in telemetry_data[satellite_id]:
                 print(satellite_id, field, "field present")
+                # Check nested positioning data
+                if field == "position":
+                    for nested_key in ["x_km", "y_km", "z_km"]:
+                        if nested_key in telemetry_data[satellite_id]["position"]:
+                            print(f"{satellite_id} {nested_key} inside {field} field present")
+                        else:
+                            print(f"{satellite_id} {nested_key} inside {field} field missing")
+            # Add logic here for anomaly checks?
             else:
                 print(satellite_id, field, "field absent")
     else:
